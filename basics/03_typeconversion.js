ddbec7ed0f1b6131e49be6d6ab735d898db5095c
Adnan-2005 <adnanahmadansari002@gmail.com>
2025-09-16 12:14:49 +0000

    container config done

---
+++ basics/03_typeconversion.js
@@ -0,0 +1,20 @@
+// let score = 33
+// if score = "33abc" => NaN => Not a number
+// if score = null => 0
+// if score = true => 1
+// if score = undefined => NaN
+// if score = "..." => NaN
+// console.log(score)
+// console.log(typeof (score))
+
+// let valueInNumber = Number(score) 
+// console.log(valueInNumber)
+// console.log(typeof (valueInNumber))
+
+let isLoggedIn = "adnan"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+// 1 => true
+// 0 => false
+// "adnan" => true
+// "" => false

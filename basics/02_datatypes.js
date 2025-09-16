0a0e479ca47c33e52538bdf6e5732d86cf921eb8
Adnan-2005 <adnanahmadansari002@gmail.com>
2025-09-16 11:47:23 +0000

    container config done

---
+++ basics/02_datatypes.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js as new version 
+// alert(3+3) // we are node js not browser 
+
+//datatypes
+// let name = "adnan" //string
+// let age= 18 //int 
+// let isloggedIn = true // boolean
+
+// number range = 2^53
+// bigint 
+// string
+// boolean
+// null => standalone value
+// undefined 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined) // output undefined
+console.log(typeof null) // output object

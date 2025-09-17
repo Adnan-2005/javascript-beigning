ac4e481bf034da5b5847adc2c323ce312bd4d02a
Adnan-2005 <adnanahmadansari002@gmail.com>
2025-09-17 16:54:29 +0000

    container config done

--- basics/03_typeconversion.js
+++ basics/03_typeconversion.js
@@ -11,10 +11,42 @@
 // console.log(valueInNumber)
 // console.log(typeof (valueInNumber))
 
-let isLoggedIn = "adnan"
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// let isLoggedIn = "adnan"
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
 // 1 => true
 // 0 => false
 // "adnan" => true
 // "" => false
+
+// operation....
+// let value = 3
+// let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+20);
+// console.log(10+2);
+// console.log(2*7);
+// console.log(2**3);
+// many more operators 
+
+// let str1 = "Hello" 
+// let str2 = " Adnan"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log(1+"2")
+// console.log("2" + 1);
+// console.log(1+2+"3");
+
+let gameC = 100
+++gameC
+console.log(gameC);
+
+
+
+
+
+
+
+

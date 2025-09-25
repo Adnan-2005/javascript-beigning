6886c9c5e5089cb157a17b8e32ec084ba8b161fa
Adnan-2005 <adnanahmadansari002@gmail.com>
2025-09-25 16:10:01 +0000

    container config done

---
+++ basics/05_string.js
@@ -0,0 +1,34 @@
+const name = "Adnan"
+const repo = 50
+// console.log(`My name is ${name} and repo count is ${repo}`);
+
+const gameName = new String(`adnan`)
+// console.log(gameName);
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(4));
+// console.log(gameName.indexOf`n`);
+
+const newString = gameName.substring(2,5)
+// console.log(newString);
+
+const anotherString = gameName.slice(-1,-6)
+// console.log(anotherString)
+
+const newStringone = "  ahmad   "
+// console.log(newStringone);
+// console.log(newStringone.trim());
+
+const url = "https://adnan.com/ahmad%5ansari"
+console.log(url.replace('%5', '-'))
+console.log(url.includes('ansari'));
+
+
+
+
+
+
+
+

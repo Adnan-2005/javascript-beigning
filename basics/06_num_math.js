9579d582352c0b685603c25e8c5f40ff13fbca59
Adnan-2005 <adnanahmadansari002@gmail.com>
2025-09-27 14:38:49 +0000

    container config done

---
+++ basics/06_num_math.js
@@ -0,0 +1,43 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+const otherNum = 23.89
+// console.log(otherNum.toPrecision(2));
+
+const hundred = 100000
+// console.log(hundred.toLocaleString('en-IN'));
+
+//........MATHS.......
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.floor(8.9));
+// console.log(Math.ceil(4.5));
+// console.log(Math.min(2,4,5,6,7,8));
+// console.log(Math.max(3.56,78,9));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20 
+console.log(Math.floor(Math.random()* (max -min +1 ))+min);
+
+
+
+
+
+
+
+
+
+
+
+
+
+

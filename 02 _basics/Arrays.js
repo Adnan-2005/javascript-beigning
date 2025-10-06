1cbd003a0704a9c4b0ee9da07ed3d34c228ba33f
Adnan-2005 <adnanahmadansari002@gmail.com>
2025-10-06 11:52:29 +0000

    container config done

---
+++ 02 _basics/Arrays.js
@@ -0,0 +1,37 @@
+// Arrays
+
+const myArr = [1,2,3,4,5]
+const arr = ["adnan", "ahmad", "ansari"]
+// console.log(myArr[0]);
+
+const Arr = new Array(1,3,4,5)
+
+// arrays methods 
+// myArr.push(6)
+// myArr.pop() //Removes the last element
+
+// myArr.unshift(9) // insert at first index 
+// myArr.shift()
+// console.log(myArr.includes(9));
+// console.log(myArr.indexOf(9));
+
+// const newArr = myArr.join()
+// console.log(myArr);
+// console.log(typeof(newArr));
+
+// slice , splice
+console.log("A ", myArr);
+const myn1 = myArr.slice(1,3)
+
+console.log(myn1);
+console.log("B ", myArr);
+
+const myn2 = myArr.splice(1,3)
+console.log("C ", myArr);
+console.log(myn2);
+ 
+
+
+
+
+

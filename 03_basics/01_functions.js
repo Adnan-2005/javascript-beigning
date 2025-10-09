5b4b24f4ca35bc6facfcc846ab9b7b294b051d08
Adnan-2005 <adnanahmadansari002@gmail.com>
2025-10-09 14:31:54 +0000

    done

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -19,4 +19,29 @@ function LoginUserMessage(username = "addy") {
     return `${username} just logged in `
 }
 // console.log(LoginUserMessage('Adnan'))
-console.log(LoginUserMessage("Adnan"))
+// console.log(LoginUserMessage("Adnan"))
+
+function calculatecartprice(val1 , vla2, ...num) {
+    return num
+
+}
+// console.log(calculatecartprice(200,100,300));
+const user = {
+    username: "Adnan",
+    price:"199"
+}
+function handleObject(anyobject) {
+    // console.log(`Username is ${anyobject.username} and price is ${anyobject.price} `);
+    
+}
+handleObject({
+    username: "addy",
+    price: 123
+})
+
+const newArray = [100 , 200 , 300 , 400]
+function returnsecondvalue(getArray) {
+    return getArray
+}
+console.log(returnsecondvalue(newArray));
+
---
+++ 03_basics/02_scope.js
@@ -0,0 +1,11 @@
+// var c = 300
+let b = 80
+if(true) {
+    let a = 10
+    const b =20
+    // var c = 30
+    console.log("Inneer b :", b);
+    
+
+}
+console.log(b);

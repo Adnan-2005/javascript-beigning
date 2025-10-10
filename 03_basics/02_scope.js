437c075083ab7026b55aeda7a9c9eb587e8c189f
Adnan-2005 <adnanahmadansari002@gmail.com>
2025-10-10 15:47:20 +0000

    done

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -4,8 +4,47 @@ if(true) {
     let a = 10
     const b =20
     // var c = 30
-    console.log("Inneer b :", b);
+    // console.log("Inneer b :", b);
     
 
 }
-console.log(b);
+// console.log(b);
+
+function one() {
+    const username = "Adnan"
+
+    function two() {
+        const website = "youtube"
+        // console.log(username);
+        
+    }
+    // console.log(website);
+    
+    // two()
+}
+// one()
+
+if(true) {
+    const username = "Adnan"
+    if(username === "Adnan") {
+        const website = " youtube"
+        // console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+}
+// console.log(username);
+
+//+++++ intresting +++++
+
+console.log(addOne(5))
+function addOne(num) {
+    return num+1
+}
+
+// console.log(addTwo(3));
+
+const addTwo = function(num ) {
+    return num + 2
+}

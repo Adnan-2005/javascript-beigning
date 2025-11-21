394d18f6ec7b19b916e63d161cff13276093f97a
Adnan-2005 <adnanahmadansari002@gmail.com>
2025-11-21 19:00:15 +0000

    done

--- 04_control_flow/one.js
+++ 04_control_flow/one.js
@@ -1,8 +1,42 @@
 //IF
 const isUerLoggedIn = true
-if(2==="2") {
-   console.log("executed");
-}
+const temp = 50
+// if(temp === 45) {
+//    console.log("executed");
+// } else {
+//    console.log("not executed");
+   
+// }
+// console.log("Done");
 
 //comparison operator
 // < , > , <= , >= , == , != , === , !==
+
+// const score = 100
+// if(score > 90) {
+//    let power = "fly"
+//    console.log(`User power: ${power}`);
+// }
+// console.log(`User power: ${power}`);
+
+const balance = 1000
+// if(balance > 500) console.log("test");
+// if(balance < 500) {
+//    console.log("less than 750");
+// } else if(balance < 600) {
+//    console.log("less than 750");
+   
+// } else {
+//    console.log("less than 1200");
+// }
+
+const userLoggedIn = true
+const debitcard = true
+const LoggedInFromGoogle = false
+const LoggedInFromMail =  true
+if(userLoggedIn && debitcard && 4==5) {
+   console.log("Allow to buy course");
+}
+if(LoggedInFromGoogle || LoggedInFromMail || guessUser) {
+   console.log("User Logged In");
+}

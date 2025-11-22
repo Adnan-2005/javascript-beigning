3a8eaf570c8476387992e4df9ce19e08043962af
Adnan-2005 <adnanahmadansari002@gmail.com>
2025-11-22 12:17:03 +0000

    done

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,39 @@
+const userEmail = []
+// if(userEmail) {
+//     console.log("Got user email");
+// } else {
+//     console.log("Dont have user email");
+// }
+
+//falsy value
+// => flase, 0 , -0 , BigInt 0n, "" ,null , undefined , NaN
+
+//truthy values
+// => ture , "0" 'flase' , " ", [] , {} , function(){}
+
+// if (userEmail.length === 0 ) {
+//     console.log("Array is empty");
+    
+// }
+
+const emptyObj = {}
+if (Object.keys(emptyObj).length===0) {
+    console.log("Object is Empty");
+    
+}
+
+// Nullish Coalescing Operator (??) : Null , undefined 
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+val1 = undefined ?? 10
+// console.log(val1);
+
+// Ternary Operator
+// condition ? true ? false
+const teaprice = 100
+teaprice <=90 ? console.log("less than 80") : console.log("more than 80");
+
+
+
--- 04_control_flow/two.js
+++ 04_control_flow/two.js
@@ -1,7 +1,27 @@
 // SWITCH 
-switch(key) {
-    case value:
+// switch(key) {
+//     case value:
+//         break;
+//     default:
+//         break;
+// }
+
+const month = 3
+switch (month) {
+    case 1:
+        console.log("January");
         break;
+    case 2:
+        console.log("Feb");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
     default:
+        console.log("not valid");
         break;
 }

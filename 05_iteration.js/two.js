b5605b2a5bb0e80b51a1b358b19cc0091c1fc7b6
Adnan-2005 <adnanahmadansari002@gmail.com>
2025-11-23 12:48:12 +0000

    do while done

---
+++ 05_iteration.js/two.js
@@ -0,0 +1,21 @@
+// WHILE LOOP
+
+let i = 0
+while (i<=10) {
+    //console.log(`Value of i is : ${i}`);
+    i += 2
+}
+
+let myArray = ["Flash" , "Batman" , "Superman"]
+let arr = 0
+while (arr<myArray.length) {
+    //console.log(`Vlaue is ${myArray[arr]}`);
+    arr++
+}
+
+// DO WHILE LOOP
+let score = 1 
+do {
+    console.log(`Score is : ${score}`);
+    score++
+} while (score<=10);
